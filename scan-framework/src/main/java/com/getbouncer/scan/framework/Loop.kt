791c791708f28507570fee70f363589e75da96c5
Adam Wushensky <adam@getbouncer.com>
2020-06-09 17:27:17 -0700

    Move images out of framework

--- scan-framework/src/main/java/com/getbouncer/scan/framework/Loop.kt
+++ scan-framework/src/main/java/com/getbouncer/scan/framework/Loop.kt
@@ -4,24 +4,18 @@ import com.getbouncer.scan.framework.time.Clock
 import com.getbouncer.scan.framework.time.ClockMark
 import com.getbouncer.scan.framework.time.Duration
 import com.getbouncer.scan.framework.time.measureTime
-import com.getbouncer.scan.framework.time.milliseconds
-import com.getbouncer.scan.framework.time.min
-import com.getbouncer.scan.framework.time.seconds
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.cancel
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.isActive
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.sync.Mutex
 import kotlinx.coroutines.sync.withLock
 import kotlinx.coroutines.withContext
-import java.util.LinkedList
 import java.util.concurrent.atomic.AtomicBoolean
 import java.util.concurrent.atomic.AtomicInteger
-
-private val MAX_ANALYZER_DELAY = 1.seconds
-private val MIN_ANALYZER_DELAY = 50.milliseconds
 
 data class LoopState<State>(
     val startedAt: ClockMark?,
@@ -53,11 +47,8 @@ sealed class AnalyzerLoop<DataFrame, State, Output>(
     initialState: State
 ) : StateUpdatingResultHandler<DataFrame, LoopState<State>, Output> {
     private val started = AtomicBoolean(false)
-    private val channel by lazy { Channel<DataFrame>(calculateChannelBufferSize()) }
 
-    private val startMutex = Mutex()
     private val cancelMutex = Mutex()
-    private val executionDurationMutex = Mutex()
 
     private var state = LoopState(
         startedAt = null,
@@ -65,35 +56,26 @@ sealed class AnalyzerLoop<DataFrame, State, Output>(
         state = initialState
     )
 
-    private val analyzerExecutionDurations = LinkedList<Duration>()
-    protected var averageAnalyzerExecutionDuration = MIN_ANALYZER_DELAY
-        private set
-
     internal abstract val name: String
 
     private lateinit var loopExecutionStatTracker: StatTracker
 
-    abstract fun calculateChannelBufferSize(): Int
-
-    open suspend fun processFrame(frame: DataFrame) = if (shouldReceiveNewFrame(state)) channel.offer(frame) else false
-
-    abstract suspend fun shouldReceiveNewFrame(state: LoopState<State>): Boolean
-
     private val updateState: (LoopState<State>) -> Unit = { state = it }
 
-    open suspend fun start(processingCoroutineScope: CoroutineScope) {
-        startMutex.withLock {
-            if (state.startedAt == null) {
-                updateState(state.copy(startedAt = Clock.markNow()))
-            } else {
-                return
-            }
+    internal fun subscribeToChannel(
+        channel: Channel<DataFrame>,
+        processingCoroutineScope: CoroutineScope
+    ) {
+        if (!started.getAndSet(true)) {
+            updateState(state.copy(startedAt = Clock.markNow()))
+        } else {
+            return
         }
 
         loopExecutionStatTracker = Stats.trackTask("loop_execution:$name")
 
         if (analyzerPool.analyzers.isEmpty()) {
-            loopExecutionStatTracker.trackResult("canceled")
+            runBlocking { loopExecutionStatTracker.trackResult("canceled") }
             onAnalyzerFailure(NoAnalyzersAvailableException)
             return
         }
@@ -102,7 +84,7 @@ sealed class AnalyzerLoop<DataFrame, State, Output>(
             val workerScope = this
             analyzerPool.analyzers.forEachIndexed { index, analyzer ->
                 launch(Dispatchers.Default) {
-                    startWorker(workerScope, index, analyzer)
+                    startWorker(channel, workerScope, index, analyzer)
                 }
             }
         }
@@ -112,29 +94,28 @@ sealed class AnalyzerLoop<DataFrame, State, Output>(
      * Launch a worker coroutine that has access to the analyzer's `analyze` method and the result handler
      */
     private suspend fun startWorker(
+        channel: Channel<DataFrame>,
         workerScope: CoroutineScope,
         workerId: Int,
         analyzer: Analyzer<DataFrame, State, Output>
     ) {
         for (frame in channel) {
             val stat = Stats.trackRepeatingTask("analyzer_execution:$name:${analyzer.name}")
-            updateAnalyzerExecutionDuration(
-                measureTime {
-                    try {
-                        val analyzerResult = analyzer.analyze(frame, state.state)
+            measureTime {
+                try {
+                    val analyzerResult = analyzer.analyze(frame, state.state)
 
-                        try {
-                            onResult(analyzerResult, state, frame, updateState)
-                        } catch (t: Throwable) {
-                            stat.trackResult("result_failure")
-                            handleResultFailure(workerScope, t)
-                        }
+                    try {
+                        onResult(analyzerResult, state, frame, updateState)
                     } catch (t: Throwable) {
-                        stat.trackResult("analyzer_failure")
-                        handleAnalyzerFailure(workerScope, t)
+                        stat.trackResult("result_failure")
+                        handleResultFailure(workerScope, t)
                     }
+                } catch (t: Throwable) {
+                    stat.trackResult("analyzer_failure")
+                    handleAnalyzerFailure(workerScope, t)
                 }
-            )
+            }
 
             cancelMutex.withLock {
                 if (state.finished && workerScope.isActive) {
@@ -146,22 +127,6 @@ sealed class AnalyzerLoop<DataFrame, State, Output>(
             stat.trackResult("success")
         }
     }
-
-    private suspend fun updateAnalyzerExecutionDuration(newDuration: Duration) =
-        executionDurationMutex.withLock {
-            analyzerExecutionDurations.add(newDuration)
-            if (analyzerExecutionDurations.size > analyzerPool.analyzers.size && analyzerExecutionDurations.size > 0) {
-                analyzerExecutionDurations.removeFirst()
-            }
-
-            averageAnalyzerExecutionDuration = when {
-                analyzerExecutionDurations.size > 1 ->
-                    analyzerExecutionDurations.reduce { a, b -> a + b } / analyzerExecutionDurations.size
-                analyzerExecutionDurations.size == 1 ->
-                    analyzerExecutionDurations.first
-                else -> MIN_ANALYZER_DELAY
-            }
-        }
 
     private suspend fun handleAnalyzerFailure(workerScope: CoroutineScope, t: Throwable) {
         if (withContext(Dispatchers.Main) { onAnalyzerFailure(t) }) {
@@ -194,7 +159,7 @@ sealed class AnalyzerLoop<DataFrame, State, Output>(
  * If the enqueued data exceeds the allowed memory size, the bottom of the data stack will be dropped and will not be
  * processed. This alleviates memory pressure when producers are faster than the consuming analyzer.
  *
- * Any data enqueued via [processFrame] will be dropped once this loop has terminated.
+ * Any data enqueued via [receiveData] will be dropped once this loop has terminated.
  *
  * @param analyzerPool: A pool of analyzers to use in this loop.
  * @param resultHandler: A result handler that will be called with the results from the analyzers in this loop.
@@ -215,28 +180,9 @@ class ProcessBoundAnalyzerLoop<DataFrame, State, Output>(
     onResultFailure,
     initialState
 ) {
-    private val shouldReceivedFrameMutex = Mutex()
-    private var lastFrameReceivedAt: ClockMark? = null
-
-    /**
-     * Determine if a new frame should be processed. This method will stagger frames based on the amount of time the
-     * analyzers take to execute. Staggering the frames evens out the rate at which they are processed.
-     */
-    override suspend fun shouldReceiveNewFrame(state: LoopState<State>): Boolean =
-        shouldReceivedFrameMutex.withLock {
-            val running = state.startedAt != null && !state.finished
-            val analyzerDelay = min(averageAnalyzerExecutionDuration, MAX_ANALYZER_DELAY) / analyzerPool.desiredAnalyzerCount
-            val shouldReceiveNewFrame = running && lastFrameReceivedAt?.elapsedSince() ?: Duration.INFINITE > analyzerDelay
-            if (shouldReceiveNewFrame) {
-                this.lastFrameReceivedAt = Clock.markNow()
-            }
-            shouldReceiveNewFrame
-        }
-
-    /**
-     * Do not queue frames since we want to process them as immediately as possible.
-     */
-    override fun calculateChannelBufferSize(): Int = Channel.RENDEZVOUS
+    fun subscribeTo(channel: Channel<DataFrame>, processingCoroutineScope: CoroutineScope) {
+        subscribeToChannel(channel, processingCoroutineScope)
+    }
 
     override suspend fun onResult(
         result: Output,
@@ -274,35 +220,19 @@ class FiniteAnalyzerLoop<DataFrame, State, Output>(
     onResultFailure,
     initialState
 ) {
-    private val processFrameMutex = Mutex()
-
-    private val framesToProcess: AtomicInteger = AtomicInteger(0)
     private val framesProcessed: AtomicInteger = AtomicInteger(0)
 
-    override suspend fun start(processingCoroutineScope: CoroutineScope) {
-        val framesToProcess = frames.map { processFrame(it) }.any()
-        if (framesToProcess) {
-            super.start(processingCoroutineScope)
+    fun start(processingCoroutineScope: CoroutineScope) {
+        val channel = Channel<DataFrame>(capacity = frames.size)
+        val hasFramesToProcess = frames.map { channel.offer(it) }.any()
+        if (hasFramesToProcess) {
+            super.subscribeToChannel(channel, processingCoroutineScope)
         } else {
-            resultHandler.onAllDataProcessed()
-        }
-    }
-
-    override suspend fun processFrame(frame: DataFrame): Boolean = processFrameMutex.withLock {
-        if (framesToProcess.get() < frames.size) {
-            val result = super.processFrame(frame)
-            if (result) {
-                framesToProcess.incrementAndGet()
+            processingCoroutineScope.launch {
+                resultHandler.onAllDataProcessed()
             }
-            result
-        } else {
-            false
         }
     }
-
-    override fun calculateChannelBufferSize(): Int = frames.size
-
-    override suspend fun shouldReceiveNewFrame(state: LoopState<State>): Boolean = state.startedAt == null
 
     override suspend fun onResult(
         result: Output,
