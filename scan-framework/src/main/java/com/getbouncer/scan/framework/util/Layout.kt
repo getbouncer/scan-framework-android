706d462d8ab00c9db20fed28bbfa1ce0f7535d6c
Steven Liu <steven@getbouncer.com>
2020-06-16 22:26:25 -0700

    add some convenience functions

--- scan-framework/src/main/java/com/getbouncer/scan/framework/util/Layout.kt
+++ scan-framework/src/main/java/com/getbouncer/scan/framework/util/Layout.kt
@@ -1,6 +1,7 @@
 package com.getbouncer.scan.framework.util
 
 import android.graphics.Rect
+import android.graphics.RectF
 import android.util.Size
 import androidx.annotation.CheckResult
 import kotlin.math.min
@@ -61,3 +62,23 @@ fun Size.scaleAndCenterWithin(containingSize: Size): Rect {
         /* bottom */ top + scaledSize.height
     )
 }
+
+fun RectF.scaled(scaledSize: Size): RectF {
+    return RectF(
+        this.left * scaledSize.width,
+        this.top * scaledSize.height,
+        this.right * scaledSize.width,
+        this.bottom * scaledSize.height
+    )
+}
+
+fun RectF.centerScaled(scaleX: Float, scaleY: Float): RectF {
+    return RectF(
+        this.centerX() - this.width() * scaleX / 2,
+        this.centerY() - this.height() * scaleY / 2,
+        this.centerX() + this.width() * scaleX / 2,
+        this.centerY() + this.height() * scaleY / 2
+    )
+}
+
+fun Rect.size() = Size(width(), height())
